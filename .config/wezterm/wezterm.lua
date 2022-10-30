c8b88328cc8942fda476538e506439232f57e7b7
strglp <contact@strangeloop.blog>
2022-10-29 22:07:54 -0400

    reduce terminal fontzie to avoid extra padding

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -28,18 +28,19 @@ end)
 
 return {
     window_padding = {
-    left = 2,
-    right = 2,
+    left = 0,
+    right = 0,
     top = 0,
     bottom = 0,
   },
   audible_bell = "Disabled",
   hide_tab_bar_if_only_one_tab = true,
+  enable_scroll_bar = false,
   window_close_confirmation = "NeverPrompt",
   window_background_opacity = 0.95,
   window_decorations = "RESIZE",
   --color_scheme = "ModifiedPencilDark",
   --color_scheme = "ModifiedPencilLight",
   font = wezterm.font('JetBrains Mono'),
-  font_size = 16,
+  font_size = 15,
 }
